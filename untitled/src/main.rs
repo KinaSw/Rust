a14881851c954398060c972e26c8adc3e28cde9e
Kinga <kingasw27@gmail.com>
2025-04-06 16:12:51 +0200

    wip

--- untitled/src/main.rs
+++ untitled/src/main.rs
@@ -6,6 +6,7 @@ fn czy_przestepny(rok: u32) -> u32{
         1
     }
 }
+
 //z2
 fn liczba_dni(miesiac: u32, rok: u32) -> u32{
     if miesiac == 2{
@@ -23,11 +24,13 @@ fn liczba_dni(miesiac: u32, rok: u32) -> u32{
     }
 
 }
+
 //z3
 fn c_na_f( c: f32) -> f32{
     let f = 32.0 + 9.0/5.0 * c;
     f
 }
+
 //z4
 fn f_na_c(f: f32)-> f32{
 
@@ -37,12 +40,12 @@ fn f_na_c(f: f32)-> f32{
 }
 //z5
 fn czas(){
-    let mut h1 = 13;
-    let mut m1 = 28;
-    let mut s1= 45;
-    let mut h2 = 15;
-    let mut m2 = 13;
-    let mut s2= 50;
+    let h1 = 13;
+    let m1 = 28;
+    let s1= 45;
+    let h2 = 15;
+    let m2 = 13;
+    let s2= 50;
 
     let t1 = 3600* h1 + 60 * m1 + s1;
     let t2 = 3600 * h2 + 60 * m2 + s2;
@@ -57,7 +60,6 @@ fn czas(){
 }
 
 //z6
-
 fn silnia(num: u32) -> u32{
     let mut wynik=1;
     for i in 2..=num{
@@ -65,6 +67,50 @@ fn silnia(num: u32) -> u32{
     }
     wynik
 }
+
+//z7
+fn cyfry( mut num: i32){
+    if num > 0{
+        loop {
+            let cyfra = num %10;
+            num/=10;
+            if cyfra == 0 {break}
+            println!("{}", cyfra);
+
+        }
+    }else {
+        num*=-1;
+        loop {
+            let cyfra = num %10;
+            num/=10;
+            if cyfra == 0 {break}
+            println!("{}", cyfra);
+
+        }
+        println!("-");
+    }
+
+}
+//z8
+fn suma(mut num : i32) -> i32{
+    let mut suma=0;
+    if num < 0{
+        num*=-1;
+        loop {
+            suma+=num%10;
+            num/=10;
+            if num == 0 {break}
+        }
+    }else {
+        loop {
+            suma+=num%10;
+            num/=10;
+            if num == 0 {break}
+        }
+    }
+    suma
+}
+
 fn main(){
 
     //let rok = 2023;
@@ -73,5 +119,8 @@ fn main(){
     //println!("{}",f_na_c(86.0));
     //println!("{}",c_na_f(30.0));
     //println!("{}", dni);
-    println!("{}", silnia(6));
+    //println!("{}", silnia(6));
+    println!("{}", suma(-12345));
+
+    //cyfry(-3672);
 }
