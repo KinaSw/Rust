41214dbc51dd1d9289c079fcf3b856fcff13aced
Kinga Świątek <163517470+KinaSw@users.noreply.github.com>
2025-07-20 00:31:15 +0200

    Delete

--- untitled1/target/debug/deps/untitled1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\kinga\Desktop\Rust\untitled1\target\debug\deps\untitled1.exe: src\main.rs
-
-C:\Users\kinga\Desktop\Rust\untitled1\target\debug\deps\untitled1.d: src\main.rs
-
-src\main.rs:
--- untitled1/target/debug/untitled1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\kinga\Desktop\Rust\untitled1\target\debug\untitled1.exe: C:\Users\kinga\Desktop\Rust\untitled1\src\main.rs
