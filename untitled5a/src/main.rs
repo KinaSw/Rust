ea5293e477a4453876da1c7c468b7b0588016cfc
Kinga Świątek <163517470+KinaSw@users.noreply.github.com>
2025-07-20 00:33:49 +0200

    Update main.rs

--- untitled5a/src/main.rs
+++ untitled5a/src/main.rs
@@ -78,26 +78,26 @@ fn wartosc_syst8_v2(z: &str) -> Option<u8> {
 fn main() {
 
     assert_eq!(zamien_syst8_na_syst2("17"), Some("1111".to_string())); // 1→001, 7→111 → 001111 → "1111"
-    assert_eq!(zamien_syst8_na_syst2("0"), Some("0".to_string()));      // zero
+    assert_eq!(zamien_syst8_na_syst2("0"), Some("0".to_string()));
     assert_eq!(zamien_syst8_na_syst2("123"), Some("1010011".to_string()));
     assert_eq!(zamien_syst8_na_syst2(""), None);                        // pusty string
-    assert_eq!(zamien_syst8_na_syst2("89"), None);                     // 9 to nie cyfra ósemkowa, debilu
-    assert_eq!(zamien_syst8_na_syst2("1a"), None);                     // a to co, niby szesnastkowe?
+    assert_eq!(zamien_syst8_na_syst2("89"), None);                    
+    assert_eq!(zamien_syst8_na_syst2("1a"), None);                     
 
     assert_eq!(wartosc_syst2("0"), Some(0));
     assert_eq!(wartosc_syst2("1"), Some(1));
     assert_eq!(wartosc_syst2("1010"), Some(10));
     assert_eq!(wartosc_syst2("11111111"), Some(255));
-    assert_eq!(wartosc_syst2("100000000"), None); // 9 bitów? WON
-    assert_eq!(wartosc_syst2(""), None); // puste? Też won
-    assert_eq!(wartosc_syst2("10102"), None); // 2? spierdalaj
+    assert_eq!(wartosc_syst2("100000000"), None);
+    assert_eq!(wartosc_syst2(""), None);
+    assert_eq!(wartosc_syst2("10102"), None);
 
-    assert_eq!(wartosc_syst8("10"), Some(8));      // 1*8 + 0 = 8
-    assert_eq!(wartosc_syst8("377"), Some(255));   // max dla u8
-    assert_eq!(wartosc_syst8("400"), None);        // 256 – za dużo
-    assert_eq!(wartosc_syst8("xyz"), None);        // co to kurwa ma być
-    assert_eq!(wartosc_syst8(""), None);           // pustka, smutek, ból
+    assert_eq!(wartosc_syst8("10"), Some(8));
+    assert_eq!(wartosc_syst8("377"), Some(255));
+    assert_eq!(wartosc_syst8("400"), None);
+    assert_eq!(wartosc_syst8("xyz"), None);
+    assert_eq!(wartosc_syst8(""), None);
 
-    assert_eq!(wartosc_syst8_v2("10"), Some(8));      // 1*8 + 0 = 8
-    assert_eq!(wartosc_syst8_v2("377"), Some(255));   // max dla u8
+    assert_eq!(wartosc_syst8_v2("10"), Some(8));
+    assert_eq!(wartosc_syst8_v2("377"), Some(255));
 }
